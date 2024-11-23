90fe4c414527c73bbea80353417c30d91ee6f16a
Nora <129881120+Remove-Darck@users.noreply.github.com>
2024-11-23 16:24:32 +0330

    Jk
    know parents  and child relations

---
+++ Animal.java
@@ -0,0 +1,52 @@
+public abstract class Animal {
+
+    
+}
+abstract class  Bird extends Animal{
+
+}
+abstract class Mammal extends Animal {
+
+    
+}
+class Eagle extends Bird  {
+
+    
+}
+ class Cheetah extends Mammal  {
+
+    
+}
+ class Parrot extends Bird  {
+
+    
+}
+ class Girafe extends Mammal {
+
+    
+}
+public interface Hunter {
+
+    
+} 
+  public class  Eagle implements Hunter  {
+
+    
+}
+public class Cheetah implements Hunter  {
+
+    
+}
+public interface Prey {
+
+    
+}
+
+public class   Parrot implements Prey {
+
+    
+}
+public class   Girafe implements Prey  {
+
+    
+}
